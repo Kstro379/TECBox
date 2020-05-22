b5c8be4a67b5bfa0b8ea31538d01dc9f788b98d5
Olman Francisco Castro Hernández <olcastro@estudiantec.cr>
2020-05-21 18:55:38 -0600

    Correción de versión
    Se corrige la versión del documento

--- TECBox-VistaCliente/TECbox/src/app/app.module.ts
+++ TECBox-VistaCliente/TECbox/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Route } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
